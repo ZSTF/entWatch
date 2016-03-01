ZSTF/entWatch

commit 71354d815ad1f09d464d574931ca828adc0e7f20

locked buttons can not be used

addons/sourcemod/scripting/entWatch.sp
@@ -1302,6 +1302,9 @@ public Action:OnButtonUse(button, activator, caller, UseType:type, Float:value)
 {
 	if (g_bConfigLoaded && !g_bRoundTransition && IsValidEdict(button))
 	{
+		if (GetEntProp(button, Prop_Data, "m_bLocked", 1))
+			return Plugin_Handled;
+
 		for (new index = 0; index < entArraySize; index++)
 		{
 			if (entArray[index][ent_buttonid] != -1 && entArray[index][ent_buttonid] == button)

commit c72a7876d2ba130daa92899127642ebaaafc7312

fixed error introduced in 71354d815ad1f09d464d574931ca828adc0e7f20 (some "buttons" don't have an m_bLocked property, like doors....) added entWatch cfg for ze_ffvii_mako_reactor_v6_b08 and ze_doom_v1_1

addons/sourcemod/scripting/entWatch.sp
@@ -14,7 +14,7 @@
 #tryinclude <morecolors>
 #tryinclude <entWatch>
 
-#define PLUGIN_VERSION "3.7.1"
+#define PLUGIN_VERSION "3.7.3"
 #undef REQUIRE_PLUGIN
 
 //----------------------------------------------------------------------------------------------------
@@ -1302,7 +1302,8 @@ public Action:OnButtonUse(button, activator, caller, UseType:type, Float:value)
 {
 	if (g_bConfigLoaded && !g_bRoundTransition && IsValidEdict(button))
 	{
-		if (GetEntProp(button, Prop_Data, "m_bLocked", 1))
+		int iOffset = FindDataMapOffs(button, "m_bLocked");
+		if (iOffset != -1 && GetEntData(button, iOffset, 1))
 			return Plugin_Handled;
 
 		for (new index = 0; index < entArraySize; index++)

cfg/sourcemod/entwatch/maps/ZE_FFVII_Mako_Reactor_V6_B08.cfg
@@ -1,0 +1,231 @@
+"entities"
+{
+	"0"
+	{
+		"name" 				"Gravity Materia"
+		"shortname" 		"Gravity"
+		"color" 			"2A2A2A"
+		"buttonclass" 		"func_button"
+		"filtername" 		"player_gravity"
+		"hasfiltername" 	"true"
+		"blockpickup" 		"false"
+		"allowtransfer" 	"true"
+		"forcedrop" 		"true"
+		"chat" 				"true"
+		"hud" 				"true"
+		"hammerid" 			"2908185"
+		"mode" 				"2"
+		"maxuses" 			"0"
+		"cooldown" 			"75"
+		"maxamount" 		"1"
+	}
+	"1"
+	{
+		"name" 				"Electro Materia"
+		"shortname" 		"Electro"
+		"color" 			"0072FF"
+		"buttonclass" 		"func_button"
+		"filtername" 		"player_electro"
+		"hasfiltername" 	"true"
+		"blockpickup" 		"false"
+		"allowtransfer" 	"true"
+		"forcedrop" 		"true"
+		"chat" 				"true"
+		"hud" 				"true"
+		"hammerid" 			"2908013"
+		"mode" 				"1"
+		"maxuses" 			"0"
+		"cooldown" 			"0"
+		"maxamount" 		"1"
+	}
+	"2"
+	{
+		"name" 				"Fire Materia"
+		"shortname" 		"Fire"
+		"color" 			"FF0000"
+		"buttonclass" 		"func_button"
+		"filtername" 		"player_fire"
+		"hasfiltername" 	"true"
+		"blockpickup" 		"false"
+		"allowtransfer" 	"true"
+		"forcedrop" 		"true"
+		"chat" 				"true"
+		"hud" 				"true"
+		"hammerid" 			"2907860"
+		"mode" 				"2"
+		"maxuses" 			"0"
+		"cooldown" 			"75"
+		"maxamount" 		"1"
+	}
+	"3"
+	{
+		"name" 				"Ice Materia"
+		"shortname" 		"Ice"
+		"color" 			"00FFFF"
+		"buttonclass" 		"func_button"
+		"filtername" 		"player_ice"
+		"hasfiltername" 	"true"
+		"blockpickup" 		"false"
+		"allowtransfer" 	"true"
+		"forcedrop" 		"true"
+		"chat" 				"true"
+		"hud" 				"true"
+		"hammerid" 			"2907354"
+		"mode" 				"2"
+		"maxuses" 			"0"
+		"cooldown" 			"75"
+		"maxamount" 		"1"
+	}
+	"4"
+	{
+		"name" 				"Heal Materia"
+		"shortname" 		"Heal"
+		"color" 			"FFFFFF"
+		"buttonclass" 		"func_button"
+		"filtername" 		"player_heal"
+		"hasfiltername" 	"true"
+		"blockpickup" 		"false"
+		"allowtransfer" 	"true"
+		"forcedrop" 		"true"
+		"chat" 				"true"
+		"hud" 				"true"
+		"hammerid" 			"2907132"
+		"mode" 				"2"
+		"maxuses" 			"0"
+		"cooldown" 			"50"
+		"maxamount" 		"1"
+	}
+	"5"
+	{
+		"name" 				"Earth Materia"
+		"shortname" 		"Earth"
+		"color" 			"965F00"
+		"buttonclass" 		"func_button"
+		"filtername" 		"player_earth"
+		"hasfiltername" 	"true"
+		"blockpickup" 		"false"
+		"allowtransfer" 	"true"
+		"forcedrop" 		"true"
+		"chat" 				"true"
+		"hud" 				"true"
+		"hammerid" 			"2908100"
+		"mode" 				"2"
+		"maxuses" 			"0"
+		"cooldown" 			"75"
+		"maxamount" 		"1"
+	}
+	"6"
+	{
+		"name" 				"Wind Materia"
+		"shortname" 		"Wind"
+		"color" 			"04B404"
+		"buttonclass" 		"func_button"
+		"filtername" 		"player_wind"
+		"hasfiltername" 	"true"
+		"blockpickup" 		"false"
+		"allowtransfer" 	"true"
+		"forcedrop" 		"true"
+		"chat" 				"true"
+		"hud" 				"true"
+		"hammerid" 			"2906834"
+		"mode" 				"2"
+		"maxuses" 			"0"
+		"cooldown" 			"75"
+		"maxamount" 		"1"
+	}
+	"7"
+	{
+		"name" 				"Ultima Materia"
+		"shortname" 		"Ultima"
+		"color" 			"00FF00"
+		"buttonclass" 		"func_button"
+		"filtername" 		"player_ultima"
+		"hasfiltername" 	"true"
+		"blockpickup" 		"false"
+		"allowtransfer" 	"true"
+		"forcedrop" 		"true"
+		"chat" 				"true"
+		"hud" 				"true"
+		"hammerid" 			"2906602"
+		"mode" 				"4"
+		"maxuses" 			"1"
+		"cooldown" 			"20"
+		"maxamount" 		"1"
+	}
+	"8"
+	{
+		"name" 				"Aqua Materia"
+		"shortname" 		"Aqua"
+		"color" 			"9DE7EB"
+		"buttonclass" 		"func_button"
+		"filtername" 		"player_water"
+		"hasfiltername" 	"true"
+		"blockpickup" 		"false"
+		"allowtransfer" 	"true"
+		"forcedrop" 		"true"
+		"chat" 				"true"
+		"hud" 				"true"
+		"hammerid" 			"2907705"
+		"mode" 				"2"
+		"maxuses" 			"0"
+		"cooldown" 			"75"
+		"maxamount" 		"1"
+	}
+	"9"
+	{
+		"name" 				"Bio Materia"
+		"shortname" 		"Bio"
+		"color" 			"39F374"
+		"buttonclass" 		"func_button"
+		"filtername" 		"player_bio"
+		"hasfiltername" 	"true"
+		"blockpickup" 		"false"
+		"allowtransfer" 	"true"
+		"forcedrop" 		"true"
+		"chat" 				"true"
+		"hud" 				"true"
+		"hammerid" 			"2906432"
+		"mode" 				"2"
+		"maxuses" 			"0"
+		"cooldown" 			"75"
+		"maxamount" 		"1"
+	}
+	"10"
+	{
+		"name" 				"Barrier Materia"
+		"shortname" 		"Barrier"
+		"color" 			"E2EF7F"
+		"buttonclass" 		"func_button"
+		"filtername" 		"player_barrier"
+		"hasfiltername" 	"true"
+		"blockpickup" 		"false"
+		"allowtransfer" 	"true"
+		"forcedrop" 		"true"
+		"chat" 				"true"
+		"hud" 				"true"
+		"hammerid" 			"2906517"
+		"mode" 				"2"
+		"maxuses" 			"0"
+		"cooldown" 			"75"
+		"maxamount" 		"1"
+	}
+	"11"
+	{
+		"name" 				"Mimic Materia"
+		"shortname" 		"Mimic"
+		"color" 			"EC4CF4"
+		"buttonclass" 		"func_button"
+		"filtername" 		"player_mimic"
+		"hasfiltername" 	"true"
+		"blockpickup" 		"false"
+		"allowtransfer" 	"true"
+		"forcedrop" 		"true"
+		"chat" 				"true"
+		"hud" 				"true"
+		"hammerid" 			"4525325"
+		"mode" 				"2"
+		"maxuses" 			"0"
+		"cooldown" 			"75"
+		"maxamount" 		"1"
+	}
+}

cfg/sourcemod/entwatch/maps/ze_DOOM_v1_1.cfg
@@ -1,0 +1,117 @@
+"entities"
+{
+	"0"
+	{
+		"name"              "Berserker"
+		"shortname"         "Berserker"
+		"color"             "FF0000"
+		"buttonclass"       "func_button"
+		"filtername"        "berserker"
+		"hasfiltername"     "true"
+		"blockpickup"       "false"
+		"allowtransfer"     "true"
+		"forcedrop"         "true"
+		"chat"              "true"
+		"hud"               "true"
+		"hammerid"          "26895"
+		"mode"              "0"
+		"maxuses"           "0"
+		"cooldown"          "0"
+		"maxamount"         "1"
+	}
+	"1"
+	{
+		"name"              "BFG"
+		"shortname"         "BFG"
+		"color"             "0000FF"
+		"buttonclass"       "func_button"
+		"filtername"        "bfg_own"
+		"hasfiltername"     "true"
+		"blockpickup"       "false"
+		"allowtransfer"     "true"
+		"forcedrop"         "true"
+		"chat"              "true"
+		"hud"               "true"
+		"hammerid"          "26859"
+		"mode"              "0"
+		"maxuses"           "0"
+		"cooldown"          "0"
+		"maxamount"         "1"
+	}
+	"2"
+	{
+		"name"              "Megasphere"
+		"shortname"         "Megasphere"
+		"color"             "00FF00"
+		"buttonclass"       "func_button"
+		"filtername"        "megasphere"
+		"hasfiltername"     "true"
+		"blockpickup"       "false"
+		"allowtransfer"     "true"
+		"forcedrop"         "true"
+		"chat"              "true"
+		"hud"               "true"
+		"hammerid"          "26879"
+		"mode"              "0"
+		"maxuses"           "0"
+		"cooldown"          "0"
+		"maxamount"         "1"
+	}
+	"3"
+	{
+		"name"              "Soulsphere"
+		"shortname"         "Soulsphere"
+		"color"             "FFFFFF"
+		"buttonclass"       "func_button"
+		"filtername"        "soulsphere"
+		"hasfiltername"     "true"
+		"blockpickup"       "false"
+		"allowtransfer"     "true"
+		"forcedrop"         "true"
+		"chat"              "true"
+		"hud"               "true"
+		"hammerid"          "26854"
+		"mode"              "0"
+		"maxuses"           "0"
+		"cooldown"          "0"
+		"maxamount"         "1"
+	}
+	"4"
+	{
+		"name"              "Soulsphere"
+		"shortname"         "Soulsphere"
+		"color"             "FFFFFF"
+		"buttonclass"       "func_button"
+		"filtername"        "soulsphere2"
+		"hasfiltername"     "true"
+		"blockpickup"       "false"
+		"allowtransfer"     "true"
+		"forcedrop"         "true"
+		"chat"              "true"
+		"hud"               "true"
+		"hammerid"          "26740"
+		"mode"              "0"
+		"maxuses"           "0"
+		"cooldown"          "0"
+		"maxamount"         "1"
+	}
+	"5"
+	{
+		"name"              "Soulsphere"
+		"shortname"         "Soulsphere"
+		"color"             "FFFFFF"
+		"buttonclass"       "func_button"
+		"filtername"        "soulsphere3"
+		"hasfiltername"     "true"
+		"blockpickup"       "false"
+		"allowtransfer"     "true"
+		"forcedrop"         "true"
+		"chat"              "true"
+		"hud"               "true"
+		"hammerid"          "26767"
+		"mode"              "0"
+		"maxuses"           "0"
+		"cooldown"          "0"
+		"maxamount"         "1"
+	}
+}